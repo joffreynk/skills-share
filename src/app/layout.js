8cd10c0e29a56122b42cc5847cce6f76d662ba70
Joffreynk <joyjoffrey@gmail.com>
2023-07-13 21:13:12 +0200

    fix max height

--- src/app/layout.js
+++ src/app/layout.js
@@ -13,10 +13,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`container` }>
-        <NavBar />
-        {children}
-        <Footer />
+      <body >
+        <div className={`container` }>
+          <NavBar />
+          {children}
+          <Footer />
+        </div>
         </body>
     </html>
   )
