ba7d68384fb007d2424ff242a148fe06583ba900
Joffreynk <joyjoffrey@gmail.com>
2023-07-13 15:54:47 +0200

    add home page content

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,7 +3,14 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h1>home page</h1>
+      <div>
+
+      <h1>The best way to learn is to share your skills</h1>
+      <p>Join a large community, learn by sharing what you know and grow your network</p>
+      </div>
+      <div>
+        <Image src='/hero.jpg' width={500} height={500} alt='hello image' />
+      </div>
     </main>
   )
 }
