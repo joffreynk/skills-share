76423f14ea7ef3e5c83865fc97ef21465f475b6b
Joffreynk <joyjoffrey@gmail.com>
2023-07-13 16:49:55 +0200

    remove unnecessary dependacies

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,6 @@
 import NavBar from '@/components/NavBar'
 import './globals.css'
-import { Inter } from 'next/font/google'
 import Footer from '@/components/Footer'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'skills share',
