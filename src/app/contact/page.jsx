5dd8c2d5e3fd8c84205e71f55ce6197c6576dee3
Joffreynk <joyjoffrey@gmail.com>
2023-07-14 09:14:19 +0200

    fix call center picture and adjust form

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -5,16 +5,16 @@ import Image from 'next/image'
 const Contact = () => {
   return (
     <div className={styles.container}>
-      <h1>Let&apos;s keep in Touch</h1>
+      <h1 className={styles.title} >Let&apos;s keep in Touch</h1>
       <div className={styles.cardContainer} >
         <div  className={styles.item} >
           <Image fill alt='call center' src='/callcenter.jpg' />
         </div>
         <div  className={styles.item} >
           <form action="" className={styles.form}>
-            <input type="text" name="name" id="name" />
-            <input type="email" name="email" id="email" />
-            <textarea name="message" id="message" cols="30" rows="10"></textarea>
+            <input className={styles.input} placeholder='Name' type="text" name="name" id="name" />
+            <input className={styles.input} placeholder='Email' type="email" name="email" id="email" />
+            <textarea className={styles.input} placeholder='Message' name="message" id="message" cols="30" rows="10"></textarea>
             <button type="submit">Send</button>
           </form>
         </div>
