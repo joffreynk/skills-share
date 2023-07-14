54013177d188714a65879b28b54a009fcb6df5d8
Joffreynk <joyjoffrey@gmail.com>
2023-07-14 19:18:03 +0200

    add category content

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import styles from '@/styles/category.module.css'
+import CustomButton from '@/components/CustomButton'
+import Image from 'next/image'
 const Category = ({params}) => {
  
-    const cat = params.category.split('-').join(' ')
+  const cat = params.category.split('-').join(' ')
   return (
     <div className={styles.container}>
-        <h1 className={styles.title}>{cat}</h1>
+      <h2 className={styles.title}>{cat}</h2>
+      <div  className={styles.content}>
+      <div  className={styles.textContainer}>
+        <h3>Creative Mind</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius libero vel commodi quo! Delectus, eius itaque. Numquam explicabo officiis omnis pariatur, atque odio molestiae sit doloribus iusto, voluptates tempora.</p>
+        <CustomButton text='view more' />
+      </div>
+      <div  className={styles.imageContainer}>
+        <Image src='/userdesign.jpg' fill alt='category picture' />
+      </div>
+      </div>
     </div>
   )
 }
