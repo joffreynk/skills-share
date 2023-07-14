c0949cc51dc318bab90959643c155578b5646d06
Joffreynk <joyjoffrey@gmail.com>
2023-07-14 20:49:55 +0200

    add blog id sections

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
-
+import styles from '@/styles/blogId.module.css'
 const BlogPost = () => {
   return (
-    <div>BlogPost</div>
+    <div className={styles.mainContainer}>
+        <div className={styles.firstContainer}>
+        <div className={styles.introContainer}></div>
+        <div className={styles.imageContainer}></div>
+        </div>
+        <div className={styles.secondContainer}></div>
+    </div>
   )
 }
 
