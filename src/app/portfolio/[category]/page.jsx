1e1467d4a54238dd323d1a06c2dc1b219cc7ba70
Joffreynk <joyjoffrey@gmail.com>
2023-07-14 19:29:40 +0200

    add category styles

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -10,12 +10,12 @@ const Category = ({params}) => {
       <h2 className={styles.title}>{cat}</h2>
       <div  className={styles.content}>
       <div  className={styles.textContainer}>
-        <h3>Creative Mind</h3>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius libero vel commodi quo! Delectus, eius itaque. Numquam explicabo officiis omnis pariatur, atque odio molestiae sit doloribus iusto, voluptates tempora.</p>
+        <h3 className={styles.textTitle} >Creative Mind</h3>
+        <p className={styles.textDescription}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius libero vel commodi quo! Delectus, eius itaque. Numquam explicabo officiis omnis pariatur, atque odio molestiae sit doloribus iusto, voluptates tempora.</p>
         <CustomButton text='view more' />
       </div>
       <div  className={styles.imageContainer}>
-        <Image src='/userdesign.jpg' fill alt='category picture' />
+        <Image className={styles.img} src='/userdesign.jpg' fill alt='category picture' />
       </div>
       </div>
     </div>
