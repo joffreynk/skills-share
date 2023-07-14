5c4bf96b3bac31337b8f4c334fb66977d0f0621f
Joffreynk <joyjoffrey@gmail.com>
2023-07-14 20:33:50 +0200

    add blog content and some styles

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,11 +1,84 @@
-import React from 'react'
+import CustomButton from "@/components/CustomButton";
+import Image from "next/image";
+import Link from "next/link";
+import styles from "@/styles/blog.module.css";
 
 const Blog = () => {
   return (
-    <div>
-      Blog
-    </div>
-  )
-}
+    <div className={styles.container}>
+      <Link href="/" className={styles.content}>
+        <div className={styles.imageContainer}>
+          <Image
+            className={styles.img}
+            src="/userdesign.jpg"
+            width={300}
+            height={300}
+            alt="category picture"
+          />
+        </div>
+        <div className={styles.textContainer}>
+          <h3 className={styles.textTitle}>Creative Mind</h3>
+          <p className={styles.textDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius
+            libero vel commodi quo! Delectus, eius itaque. Numquam explicabo
+            officiis omnis pariatur, atque odio molestiae sit doloribus iusto,
+            voluptates tempora. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Similique nam, voluptas labore, maxime, laboriosam
+            molestias deleniti nemo voluptatibus quo esse ipsa totam minus non
+            enim iste in. Eos, quam sunt?
+          </p>
+        </div>
+      </Link>
 
-export default Blog
+      <Link href="/" className={styles.content}>
+        <div className={styles.imageContainer}>
+          <Image
+            className={styles.img}
+            src="/userdesign.jpg"
+            width={300}
+            height={300}
+            alt="category picture"
+          />
+        </div>
+        <div className={styles.textContainer}>
+          <h3 className={styles.textTitle}>Creative Mind</h3>
+          <p className={styles.textDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius
+            libero vel commodi quo! Delectus, eius itaque. Numquam explicabo
+            officiis omnis pariatur, atque odio molestiae sit doloribus iusto,
+            voluptates tempora. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Similique nam, voluptas labore, maxime, laboriosam
+            molestias deleniti nemo voluptatibus quo esse ipsa totam minus non
+            enim iste in. Eos, quam sunt?
+          </p>
+        </div>
+      </Link>
+
+      <Link href="/" className={styles.content}>
+        <div className={styles.imageContainer}>
+          <Image
+            className={styles.img}
+            src="/userdesign.jpg"
+            width={300}
+            height={300}
+            alt="category picture"
+          />
+        </div>
+        <div className={styles.textContainer}>
+          <h3 className={styles.textTitle}>Creative Mind</h3>
+          <p className={styles.textDescription}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius
+            libero vel commodi quo! Delectus, eius itaque. Numquam explicabo
+            officiis omnis pariatur, atque odio molestiae sit doloribus iusto,
+            voluptates tempora. Lorem ipsum dolor sit amet consectetur
+            adipisicing elit. Similique nam, voluptas labore, maxime, laboriosam
+            molestias deleniti nemo voluptatibus quo esse ipsa totam minus non
+            enim iste in. Eos, quam sunt?
+          </p>
+        </div>
+      </Link>
+    </div>
+  );
+};
+
+export default Blog;
--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -8,6 +8,7 @@ const Category = ({params}) => {
   return (
     <div className={styles.container}>
       <h2 className={styles.title}>{cat}</h2>
+
       <div  className={styles.content}>
       <div  className={styles.textContainer}>
         <h3 className={styles.textTitle} >Creative Mind</h3>
@@ -20,6 +21,33 @@ const Category = ({params}) => {
         <Image className={styles.img} src='/userdesign.jpg' fill alt='category picture' />
       </div>
       </div>
+    
+      <div  className={styles.content}>
+      <div  className={styles.textContainer}>
+        <h3 className={styles.textTitle} >Creative Mind</h3>
+        <p className={styles.textDescription}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius libero vel commodi quo! Delectus, eius itaque. Numquam explicabo officiis omnis pariatur, atque odio molestiae sit doloribus iusto, voluptates tempora.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique nam, voluptas labore, maxime, laboriosam molestias deleniti nemo voluptatibus quo esse ipsa totam minus non enim iste in. Eos, quam sunt?
+        </p>
+        <CustomButton text='view more' />
+      </div>
+      <div  className={styles.imageContainer}>
+        <Image className={styles.img} src='/userdesign.jpg' fill alt='category picture' />
+      </div>
+      </div>
+
+      <div  className={styles.content}>
+      <div  className={styles.textContainer}>
+        <h3 className={styles.textTitle} >Creative Mind</h3>
+        <p className={styles.textDescription}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius libero vel commodi quo! Delectus, eius itaque. Numquam explicabo officiis omnis pariatur, atque odio molestiae sit doloribus iusto, voluptates tempora.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique nam, voluptas labore, maxime, laboriosam molestias deleniti nemo voluptatibus quo esse ipsa totam minus non enim iste in. Eos, quam sunt?
+        </p>
+        <CustomButton text='view more' />
+      </div>
+      <div  className={styles.imageContainer}>
+        <Image className={styles.img} src='/userdesign.jpg' fill alt='category picture' />
+      </div>
+      </div>
+
     </div>
   )
 }
