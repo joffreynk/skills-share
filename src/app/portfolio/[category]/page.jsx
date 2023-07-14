07aa11e2b2121032ff2a311270e8b590f8c4778c
Joffreynk <joyjoffrey@gmail.com>
2023-07-14 19:53:03 +0200

    finalize category design

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -11,7 +11,9 @@ const Category = ({params}) => {
       <div  className={styles.content}>
       <div  className={styles.textContainer}>
         <h3 className={styles.textTitle} >Creative Mind</h3>
-        <p className={styles.textDescription}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius libero vel commodi quo! Delectus, eius itaque. Numquam explicabo officiis omnis pariatur, atque odio molestiae sit doloribus iusto, voluptates tempora.</p>
+        <p className={styles.textDescription}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam eius libero vel commodi quo! Delectus, eius itaque. Numquam explicabo officiis omnis pariatur, atque odio molestiae sit doloribus iusto, voluptates tempora.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique nam, voluptas labore, maxime, laboriosam molestias deleniti nemo voluptatibus quo esse ipsa totam minus non enim iste in. Eos, quam sunt?
+        </p>
         <CustomButton text='view more' />
       </div>
       <div  className={styles.imageContainer}>
