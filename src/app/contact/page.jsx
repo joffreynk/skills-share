16334c604517b88a42cbfed852474d77492648d6
Joffreynk <joyjoffrey@gmail.com>
2023-07-14 11:05:13 +0200

    fix background image

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,4 +1,5 @@
 
+import CustomButton from '@/components/CustomButton'
 import styles from '@/styles/contact.module.css'
 import Image from 'next/image'
 
@@ -8,14 +9,14 @@ const Contact = () => {
       <h1 className={styles.title} >Let&apos;s keep in Touch</h1>
       <div className={styles.cardContainer} >
         <div  className={styles.item} >
-          <Image fill alt='call center' src='/callcenter.jpg' />
+          <Image fill alt='call center' src='/callcenter.png' />
         </div>
         <div  className={styles.item} >
           <form action="" className={styles.form}>
-            <input className={styles.input} placeholder='Name' type="text" name="name" id="name" />
-            <input className={styles.input} placeholder='Email' type="email" name="email" id="email" />
+            <input className={styles.input} placeholder='Full name' type="text" name="name" id="name" />
+            <input className={styles.input} placeholder='e-mail' type="email" name="email" id="email" />
             <textarea className={styles.input} placeholder='Message' name="message" id="message" cols="30" rows="10"></textarea>
-            <button type="submit">Send</button>
+            <CustomButton text='Send' />
           </form>
         </div>
       </div>
--- src/components/CustomButton.jsx
+++ src/components/CustomButton.jsx
@@ -1,8 +1,10 @@
 import Link from 'next/link'
 import styles from '@/styles/customButton.module.css'
 
-const CustomButton = ({url, text}) => (
+const CustomButton = ({url, text}) => (url?
     <Link href={url||'/'} ><button type="button" className={styles.container}>{text}</button></Link>
+    :
+    <button type="button" className={styles.container}>{text}</button>
   )
 
 export default CustomButton
