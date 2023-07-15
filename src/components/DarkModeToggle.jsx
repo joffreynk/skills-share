02cd40110f6b3aee02afbaa569da855b5bf7dd83
Joffreynk <joyjoffrey@gmail.com>
2023-07-15 08:12:22 +0200

    add toggle mode

--- src/components/DarkModeToggle.jsx
+++ src/components/DarkModeToggle.jsx
@@ -1,11 +1,14 @@
+'use client'
 import  "@/styles/darkModeToggle.css";
+import { useState } from "react";
 
 const DarkModeToggle = () => {
+  const [mode, setMode] = useState(true)
   return (
-    <div className='darkModeContainer'>
+    <div className='darkModeContainer' onClick={()=>setMode(!mode)}>
       <div className='darkModeMoon'>🌙</div>
       <div className='darkModeSun'>☀</div>
-      <div className='darkModeBall' />
+      <div className='darkModeBall'  style={mode ? {left: '2px'} : {right: '2px'}} />
     </div>
   );
 };
