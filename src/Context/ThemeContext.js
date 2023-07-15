ef5eb75df43649a8e665524d4a481dab37e4ba51
Joffreynk <joyjoffrey@gmail.com>
2023-07-15 09:20:19 +0200

    add theme to the local storage

--- src/Context/ThemeContext.js
+++ src/Context/ThemeContext.js
@@ -5,9 +5,13 @@ import { createContext, useState } from "react";
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [mode, setMode] = useState('light')
+  const [mode, setMode] = useState((localStorage.getItem('skills-share-mode') && JSON.parse(localStorage.getItem('skills-share-mode'))) || 'light');
   const toggle = () =>{
-    setMode((prev)=>(prev ==='light' ? 'dark' : 'light'));
+    setMode((prev)=>{
+      const newMode  = prev ==='light' ? 'dark' : 'light'
+      localStorage.setItem('skills-share-mode', JSON.stringify(newMode));
+      return newMode
+    });
   }
 
   return (<ThemeContext.Provider value={{mode, toggle}}>
