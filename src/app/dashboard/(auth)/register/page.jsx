fe2fdd9453f60bd0ca0aea6dc282cff8dacca009
Joffreynk <joyjoffrey@gmail.com>
2023-07-16 23:10:06 +0200

    adjust register form design

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -4,14 +4,14 @@ import Link from 'next/link'
 const Register = () => {
   return (
     <div className={styles.container}>
-      <h2>Register</h2>
+      <h2 className={styles.title}>Register</h2>
       <form className={styles.form}>
         <input type="text" name="name" id="name" placeholder='John Doe' className={styles.input}required  />
         <input type="email" name="email" id="email" placeholder='example@gmail.com' className={styles.input} required />
         <input type="password" name="password" id="password" placeholder='password' className={styles.input}required  />
         <button type="submit" className={styles.btn}>Register</button>
       </form>
-      <Link href='/dashboard/login' >Login  with other accounts</Link>
+      <Link className={styles.link} href='/dashboard/login' >Login  with other accounts</Link>
     </div>
   )
 }
