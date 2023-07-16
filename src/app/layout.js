9509163aad2410b01ab511284f6f937242caee32
Joffreynk <joyjoffrey@gmail.com>
2023-07-16 20:53:03 +0200

    add seesion provider

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,28 +1,31 @@
-import NavBar from '@/components/NavBar'
-import './globals.css'
-import { Inter } from 'next/font/google'
-import Footer from '@/components/Footer'
-import { ThemeProvider } from '@/Context/ThemeContext'
+import NavBar from "@/components/NavBar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+import Footer from "@/components/Footer";
+import { ThemeProvider } from "@/Context/ThemeContext";
+import AuthSessiopProvider from "@/components/AuthSessiopProvider";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'skills share',
-  description: 'share your skills and improve your performance',
-}
+  title: "skills share",
+  description: "share your skills and improve your performance",
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className} >
-          <ThemeProvider>
-        <div className={`container` }>
-            <NavBar />
-            {children}
-            <Footer />
-        </div>
-          </ThemeProvider>
-        </body>
+      <body className={inter.className}>
+        <ThemeProvider>
+          <AuthSessiopProvider>
+            <div className={`container`}>
+              <NavBar />
+              {children}
+              <Footer />
+            </div>
+          </AuthSessiopProvider>
+        </ThemeProvider>
+      </body>
     </html>
-  )
+  );
 }
--- src/components/AuthSessiopProvider.jsx
+++ src/components/AuthSessiopProvider.jsx
@@ -1,7 +1,11 @@
+'use client'
+
 import { SessionProvider } from "next-auth/react";
 
 export default function AuthSessiopProvider({ children }) {
-  return <SessionProvider session={session}>
+  return <SessionProvider >
     {children}
   </SessionProvider>;
 }
+
+//<SessionProvider session={session}>
