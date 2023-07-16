771679df2cf0258bcb8735895d0f87c2127589a8
Joffreynk <joyjoffrey@gmail.com>
2023-07-16 22:58:14 +0200

    style register form

--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -4,10 +4,11 @@ import Link from 'next/link'
 const Register = () => {
   return (
     <div className={styles.container}>
+      <h2>Register</h2>
       <form className={styles.form}>
-        <input type="text" name="name" id="name" className={styles.input}required  />
-        <input type="email" name="email" id="email" className={styles.input} required />
-        <input type="password" name="password" id="password" className={styles.input}required  />
+        <input type="text" name="name" id="name" placeholder='John Doe' className={styles.input}required  />
+        <input type="email" name="email" id="email" placeholder='example@gmail.com' className={styles.input} required />
+        <input type="password" name="password" id="password" placeholder='password' className={styles.input}required  />
         <button type="submit" className={styles.btn}>Register</button>
       </form>
       <Link href='/dashboard/login' >Login  with other accounts</Link>
