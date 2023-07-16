4d7b7c732821f94f0b9dc511c527a7a1e62d45a3
Joffreynk <joyjoffrey@gmail.com>
2023-07-16 22:07:03 +0200

    try fixing social login

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,9 +1,12 @@
+'use client'
+import { signIn } from 'next-auth/react'
 import React from 'react'
 
 const Login = () => {
   return (
     <div>
-      Login
+      <button type="button" onClick={()=> signIn('google')}>Sgn in with Google</button>
+      <button type="button" onClick={()=> signIn('github')}>Sgn in with GitHub</button>
     </div>
   )
 }
--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,6 +1,13 @@
+'use client'
+
+import { useSession } from 'next-auth/react'
 import React from 'react'
 
 const Dashboard = () => {
+  const { session, status } = useSession()
+  console.log('====================================');
+  console.log(session);
+  console.log('====================================');
   return (
     <div>Dashboard</div>
   )
