73eb56e1f9f0ecd02373cafadc0863b7ab311709
Joffreynk <joyjoffrey@gmail.com>
2023-07-16 22:20:34 +0200

    create register form interface

--- src/app/dashboard/(auth)/login/page.jsx
+++ src/app/dashboard/(auth)/login/page.jsx
@@ -1,12 +1,14 @@
 'use client'
 import { signIn } from 'next-auth/react'
+import Link from 'next/link'
 import React from 'react'
 
 const Login = () => {
   return (
-    <div>
+    <div >
       <button type="button" onClick={()=> signIn('google')}>Sgn in with Google</button>
       <button type="button" onClick={()=> signIn('github')}>Sgn in with GitHub</button>
+      <Link href='/dashboard/register' >Login  with other accounts</Link>
     </div>
   )
 }
--- src/app/dashboard/(auth)/register/page.jsx
+++ src/app/dashboard/(auth)/register/page.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
-
+import styles from '@/styles/register.module.css'
+import Link from 'next/link'
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className={styles.container}>
+      <form className={styles.form}>
+        <input type="text" name="name" id="name" className={styles.input}required  />
+        <input type="email" name="email" id="email" className={styles.input} required />
+        <input type="password" name="password" id="password" className={styles.input}required  />
+        <button type="submit" className={styles.btn}>Register</button>
+      </form>
+      <Link href='/dashboard/login' >Login  with other accounts</Link>
+    </div>
   )
 }
 
