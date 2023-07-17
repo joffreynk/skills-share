2761973d2fbf60061a09ff6760e31bb7a6facc3b
Joffreynk <joyjoffrey@gmail.com>
2023-07-17 13:42:35 +0200

    add logout for logged in users

--- src/components/NavBar/index.jsx
+++ src/components/NavBar/index.jsx
@@ -1,8 +1,12 @@
+'use client'
+
 import { navBarLinks } from "@/constants"
 import Link from "next/link"
 import styles from '@/styles/navBar.module.css'
 import DarkModeToggle from "@/components/DarkModeToggle.jsx"
+import { signOut, useSession } from "next-auth/react"
 const NavBar = () => {
+  const session = useSession()
   return (
       <nav className={styles.container}>
         <div className={styles.logo}>
@@ -13,7 +17,8 @@ const NavBar = () => {
         {navBarLinks.map(link=>(
             <Link className={styles.link} key={link.id} href={link.url} >{link.title}</Link>
         ))}
-        <button type="button" className={styles.button}>Logout</button>
+        {session?.status === 'authenticated' && (<button type="button" onClick={()=>signOut()} className={styles.button}>Logout</button>)}
+        
         </div>
       </nav>
   )
