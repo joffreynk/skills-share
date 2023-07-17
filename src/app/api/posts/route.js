5977a9063f82247b17d8cfd5bef715787a9ca64d
Joffreynk <joyjoffrey@gmail.com>
2023-07-17 14:20:17 +0200

    get user posts if exists

--- src/app/api/posts/route.js
+++ src/app/api/posts/route.js
@@ -4,10 +4,12 @@ import post from "@/models/postModel.js"
 import connection from "@/utils/db.js"
 
 export const GET = async(req, res)=>{
+  const url = new URL(req.url)
+  const username = url.searchParams.get('username')
   try {
     await connection()
 
-    const posts = await post.find()
+    const posts = await post.find(username && { username})
 
     return new NextResponse(JSON.stringify(posts), {status: 200})
     
