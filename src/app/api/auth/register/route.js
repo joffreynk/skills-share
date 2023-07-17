c9cf7c9127939e657aff62e3d640120cd1a40e59
Joffreynk <joyjoffrey@gmail.com>
2023-07-17 08:21:27 +0200

    add create user api

--- src/app/api/auth/register/route.js
+++ src/app/api/auth/register/route.js
@@ -1,13 +1,20 @@
+import User from "@/models/userModel";
 import connection from "@/utils/db";
 import {bcrypt} from 'bcryptjs'
+import { NextResponse } from "next/server";
 export const POST = async(request, {params})=> {
   
   try {
     const {name, email, password} = await request.json();
     await connection();
 
-    const hashedPassword = await bcrypt.has
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({name, email, password: hashedPassword});
+    await user.save()
+
+    return new NextResponse('User is successfully created', {status : 201})
+
   } catch (error) {
-    
+    return new NextResponse(error.message, {status : 500})
   }
 }
