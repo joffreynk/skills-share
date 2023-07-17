e61eb49299f159bf3e148f1c95fd3393b15bcd5f
Joffreynk <joyjoffrey@gmail.com>
2023-07-17 15:00:13 +0200

    create posts form

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -10,9 +10,10 @@ import styles from '@/styles/dashboard.module.css'
 import useSWR from 'swr'
 
 const Dashboard = () => {
+  const [error, setError] = useState(false)
   const session = useSession();
   const router = useRouter();
-  const { data, error, isLoading } = useSWR(`/api/posts${session?.data?.name && (`/?username=${session?.data?.name}`)}`, (...args) => (fetch(...args).then((res) =>res.json())));
+  const { data, error: loadingError, isLoading } = useSWR(`/api/posts${session?.data?.name && (`/?username=${session?.data?.name}`)}`, (...args) => (fetch(...args).then((res) =>res.json())));
 
   if (session?.status === "loading")
     return <LoadingSpinner title="Skills share is autherizing you" />;
@@ -22,10 +23,33 @@ const Dashboard = () => {
   }
 
 
+  const handleSubmit = async(e) =>{
+    e.preventDefault();
+
+    const name = e.target[0].value;
+    const email = e.target[1].value;
+    const password = e.target[2].value;
+
+    try {
+      const res = await fetch('/api/auth/register', {
+        method: 'POST',
+        'contentType': 'application/json',
+        body: JSON.stringify({name, email, password})
+      });
+      error && setError(false);
+      res.ok && router.push('/dashboard/login/?message=Account successfully registered')
+    } catch (error) {
+      e.target[2].value = '';
+      setError(true)
+    }
+
+  }
+
+
   return (
     <div className={styles.container}>
       <div className={styles.posts}>
-        {data &&data.length
+        {data && data.length
           ? data.map((blog) => (
               <Link
                 key={blog._id}
@@ -49,6 +73,17 @@ const Dashboard = () => {
             ))
           : "oooops! you do't have any pos"}
       </div>
+      <div className={styles.createPosts}>
+      <h2 className={styles.title}>Register</h2>
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input type="text" name="name" id="name" placeholder='John Doe' minLength={2} className={styles.input}required  />
+        <input type="email" name="email" id="email" placeholder='example@gmail.com' className={styles.input} required />
+        <input type="password" name="password" id="password" placeholder='password' minLength={4} className={styles.input}required  />
+        <button  className={styles.btn}>Register</button>
+        {error && (<p>check your input data!</p>)}
+      </form>
+      <Link className={styles.link} href='/dashboard/login' >Login  with other accounts</Link>
+    </div>
     </div>
   );
 };
